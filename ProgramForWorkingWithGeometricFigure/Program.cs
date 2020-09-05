4fd2f6ac842cf4a7b4948462088751b5d27493a8
Nawiarra <nika.berezovskaya@ukr.net>
2020-09-05 19:47:57 +0300

    Add main program body

--- ProgramForWorkingWithGeometricFigure/Program.cs
+++ ProgramForWorkingWithGeometricFigure/Program.cs
@@ -11,7 +11,19 @@ namespace ProgramForWorkingWithGeometricFigure
     {
         static void Main(string[] args)
         {
-            
+            Square customSquare = new Square(10);
+            Console.WriteLine(customSquare.GetInfo());
+
+
+            Circle customCircle = new Circle();
+            customCircle.CreateEllips(new Point(0, 0), 6);
+            Console.WriteLine(customCircle.GetInfo());
+
+            Rectangle customRectangle = new Rectangle(5, 7);
+            Console.WriteLine(customRectangle.GetInfo());
+
+            Console.ReadKey();
+
         }
     }
 }

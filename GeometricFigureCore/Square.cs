b46160168cb6f1c6e5495770ebe2e771da40987a
Nawiarra <nika.berezovskaya@ukr.net>
2020-09-05 19:22:30 +0300

    Add Square class

---
+++ GeometricFigureCore/Square.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricFigureCore
+{
+    public class Square : FigureInterface <Square>
+    {
+        public float Side { get; private set; }
+
+        public bool Equals(Square obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetArea()
+        {
+            return Math.Pow(Side, 2);
+        }
+
+        public string GetInfo()
+        {
+            return $"This is rectangle with side = {Side} and area = {GetArea()} ";
+        }
+    }
+}
